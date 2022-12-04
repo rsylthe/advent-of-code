0c24ee20e482984b497e7e928a226dcd7f0186c6
Roy Åne Sylthe <royaane@gmail.com>
2022-12-04 01:05:29 +0100

    feat(gradle): add kotest test framework

--- buildSrc/src/main/kotlin/aokotlin.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/aokotlin.kotlin-common-conventions.gradle.kts
@@ -13,9 +13,9 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-//    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
 }
 
-tasks.named<Test>("test") {
+tasks.test {
     useJUnitPlatform()
 }
